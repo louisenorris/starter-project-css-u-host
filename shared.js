6c64f3ec08efa05f9befb90b607b3c8aa26de870
Louise Norris <louisenorris73@yahoo.co.uk>
2020-02-20 17:35:21 +0000

    added event listeners to choose plan buttons, backdrop and no button on modal

--- shared.js
+++ shared.js
@@ -1,5 +1,25 @@
 const backdrop = document.querySelector('.backdrop');
+const selectPlanButtons = document.querySelectorAll('.plan button')
+const modal = document.querySelector('.modal')
+const modalButton = document.querySelector('.modal__action--negative')
 
-// console.dir(backdrop)
+console.dir(modalButton)
 
 // backdrop.style.display = 'block'
+
+for (let i = 0; i < selectPlanButtons.length; i++ ) {
+    selectPlanButtons[i].addEventListener('click', function() {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    })
+}
+
+backdrop.addEventListener('click', function() {
+    modal.style.display = 'none';
+    backdrop.style.display = 'none';
+})
+
+modalButton.addEventListener('click', function() {
+    modal.style.display = 'none';
+    backdrop.style.display = 'none';
+})
